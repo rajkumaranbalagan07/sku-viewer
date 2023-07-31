856e05eb974648b8add9122f855c355b4606c88a
RANBALAG <ranbalag@ford.com>
2023-07-31 15:09:41 +0530

    Commit after http method change

--- pages/sku/[sku].tsx
+++ pages/sku/[sku].tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps } from "next";
 
 interface SkuData {
   sku: string;
@@ -8,7 +8,7 @@ interface SkuData {
 }
 
 const SkuPage: React.FC<{ sku: string | SkuData }> = ({ sku }) => {
-  if (typeof sku === 'string') {
+  if (typeof sku === "string") {
     return <div>{sku}</div>;
   }
 
@@ -25,31 +25,19 @@ export default SkuPage;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const skuId = context.params?.sku as string;
-  const decodedSkuId = decodeURIComponent(skuId);
-
+  const encodedSkuId = encodeURIComponent(skuId);
   const res = await fetch(
-    // 'http://127.0.0.1:4000/api/stock-tracker/v1/stock',
-    'https://vpjzb2ujzm.us-east-1.awsapprunner.com/api/stock-tracker/v1/stock',
-    {
-      method: 'POST',
-      headers: {
-        Accept: '*/*',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ sku: decodedSkuId }),
-    }
+    `https://vpjzb2ujzm.us-east-1.awsapprunner.com/api/stock-tracker/v1/stock/sku/${encodedSkuId}`
   );
 
-  if (!res.ok) {
+  if (res.status !== 200) {
     return {
       props: {
-        sku: 'SKU not found',
+        sku: "SKU not found",
       },
     };
   }
-
   const sku = await res.json();
-
   return {
     props: {
       sku,
